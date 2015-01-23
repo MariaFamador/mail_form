81022c5d4b7c38d1068eaebe174e8953bd5ed545
Maria Famador <maria.famador87@gmail.com>
2015-01-23 22:19:51 +0800

    Use attribute_method_prefix and suffix

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -1,7 +1,24 @@
 module MailForm 
   class Base
+    include ActiveModel::AttributeMethods
+    include ActiveModel::Conversion
+
+    attribute_method_prefix 'clear_' 
+    attribute_method_suffix '?'
+
     def self.attributes(*names) 
       attr_accessor(*names)
+      #it defines the prefix methods for the given attributes names
+      define_attribute_methods(names)
     end 
+
+  protected
+    def clear_attribute(attribute)
+      send("#{attribute}=", nil)
+    end
+
+    def attribute?(attribute)
+      send(attribute).present?
+    end
   end
 end
