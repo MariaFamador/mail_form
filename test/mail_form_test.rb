18ae77e0697e93c05643752014c7270e1ff26674
Maria Famador <maria.famador87@gmail.com>
2015-01-23 22:20:20 +0800

    Tests for attribute_method_prefix and suffix

--- test/mail_form_test.rb
+++ test/mail_form_test.rb
@@ -8,5 +8,27 @@ class MailFormTest < ActiveSupport::TestCase
     assert_equal "User", sample.name
     sample.email = "user@example.com" 
     assert_equal "user@example.com", sample.email
-    end
+  end
+
+  test "sample mail can clear attributes using clear_ prefix" do 
+    sample = SampleMail.new
+    sample.name = "User"
+    sample.email = "user@example.com" 
+    assert_equal "User", sample.name
+    assert_equal "user@example.com", sample.email
+
+    sample.clear_name
+    sample.clear_email
+    assert_nil sample.name
+    assert_nil sample.email
+  end
+
+  test "sample mail can ask if an attribute is present or not" do 
+    sample = SampleMail.new
+    assert !sample.name?
+    sample.name = "User" 
+    assert sample.name?
+    sample.email = ""
+    assert !sample.email?
+  end
 end
